c409a65832120c6671637a0b7d4bc6fa5c0df561
jensbri <35558560+jensbri@users.noreply.github.com>
2023-02-20 16:24:41 +1300

    sudo for desktop creation

--- runMeForEveryNewUser.sh
+++ runMeForEveryNewUser.sh
@@ -63,7 +63,7 @@ else
 fi
 
 # Create symlinks/Desktop shortcuts to improve user experience
-mkdir -p /home/${user_name}/Desktop
+sudo mkdir -p /home/${user_name}/Desktop
 if [ ! -L "/home/${user_name}/Desktop/data" ]; then
  sudo ln -s ${shared_folder} /home/${user_name}/Desktop/data
  sudo chown ${user_name}: /home/${user_name}/Desktop/data
