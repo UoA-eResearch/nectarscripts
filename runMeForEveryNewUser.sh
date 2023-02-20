ee78b56ea2dbfa5976d282a4586f39dd23608bab
jensbri <35558560+jensbri@users.noreply.github.com>
2023-02-20 16:19:23 +1300

    Update runMeForEveryNewUser.sh

--- runMeForEveryNewUser.sh
+++ runMeForEveryNewUser.sh
@@ -63,10 +63,10 @@ else
 fi
 
 # Create symlinks/Desktop shortcuts to improve user experience
-mkdir -p ${user_name}/Desktop
+mkdir -p /home/${user_name}/Desktop
 if [ ! -L "/home/${user_name}/Desktop/data" ]; then
-  ln -s ${shared_folder} /home/${user_name}/Desktop/data
-  sudo chown ${user_name}: /home/${user_name}/Desktop/data
+ sudo ln -s ${shared_folder} /home/${user_name}/Desktop/data
+ sudo chown ${user_name}: /home/${user_name}/Desktop/data
 fi
 
 tmp_files="opening_deeplabcut.odt mate-terminal.desktop"
