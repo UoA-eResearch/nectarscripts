c95ba20184821fd0a714c46ee44fd5ab42503990
jensbri <35558560+jensbri@users.noreply.github.com>
2023-02-20 13:49:46 +1300

    shuffled into one time and every new user added

---
+++ TO BE RUN FOR EVERY USER TO BE ADDED.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Name of the group everybody is in
+group_name='research'
+shared_folder='/mnt/' # THIS IS NOW POINTING DIRECTLY TO THE MOUNTED RESERACH DRIVE
+
+function display_help {
+    echo ""
+    echo "Usage $0 [-h] <username> <public_ssh_key_file>"
+    echo "  <username>: Name of the user to be created, e.g. jche159"
+    echo "  <public_ssh_key_file>: Path to the public SSH key file, matching the private key <username> is going to use to log into the VM"
+    # how do they pass that keyfile on? Do they potentialy have to create one in the first place?
+    echo "  -h: print this help message"
+    echo ""
+}
+
+# Check arguments passed in through the command-line
+if [ "$#" -lt "1" ]; then
+    echo "WARNING: No command-line argument provided"
+    display_help
+    exit 1
+elif [ "$#" -eq "1" ] && [ "$1" == "-h" ]; then
+    display_help
+    exit 0
+elif [ "$#" -eq "2" ]; then
+    user_name=$1
+    pub_ssh_key_file=$2
+    if [ ! -f "${pub_ssh_key_file}" ]; then
+        echo "Public SSH key file '${pub_ssh_key_file}' does not exist."
+        echo "Exiting..."
+      exit 1
+    fi
+elif [ "$#" -gt "2" ]; then
+    echo "WARNING: More than 2 command-line arguments provided"
+    display_help
+    exit 1
+else
+    echo "WARNING: Unexpected error"
+    exit 1
+fi
+
+# Create group if it does not already exists
+# ASK: WHAT GROUP
+if [ $(getent group "${group_name}") ]; then
+    echo "Group ${group_name} already exists."
+else
+    echo "Group ${group_name} does not yet exist. Creating..."
+    sudo groupadd ${group_name}
+fi
+
+# Create user if it does not already exists
+if [ $(getent passwd "${user_name}") ]; then
+    echo "User ${user_name} already exists."
+else
+    echo "User ${user_name} does not yet already. Creating..."
+    sudo useradd --base-dir /home --shell /bin/bash ${user_name}
+    authz_key_file="/home/${user_name}/.ssh/authorized_keys"
+    sudo mkdir -p /home/${user_name}/.ssh
+    sudo touch ${authz_key_file}
+    cat "${pub_ssh_key_file}" | sudo tee --append ${authz_key_file}
+    sudo chmod 600 ${authz_key_file}
+    sudo chmod 700 /home/${user_name}
+    sudo chown -R ${user_name}: /home/${user_name}
+    sudo usermod -a -G ${group_name} ${user_name}
+    echo "${user_name}  ALL=(ALL) NOPASSWD: ALL" | sudo tee --append /etc/sudoers
+fi
+
+# Create shared folder if it does not exist yet
+# ASK: Probably not needed if the other script has been executed once?
+if [ ! -d ${shared_folder} ]; then
+ sudo mkdir ${shared_folder}
+fi
+
+# install dependency package
+# ASK: DO WE NEED TO CUT-PASTE THAT TO THE RUN ONLY ONCE SCRIPT
+sudo dpkg -l acl > /dev/null
+if [ "$?" -gt "0" ]; then
+    sudo apt-get install acl
+fi
+
+
+
+# Create symlinks/Desktop shortcuts to improve user experience
+if [ ! -L "${user_name}/Desktop/data" ]; then 
+    "ln -s ${shared_folder} /home/${user_name}/Desktop/."
+    "sudo chown ${user_name}: /home/${user_name}/Desktop/data"
+fi
+
+if [ ! -L "${user_name}/Desktop/opening_deeplabcut.odt" ]; then 
+    "ln -s /home/ubuntu/Desktop/opening_deeplabcut.odt /home/${user_name}/Desktop/."
+    "sudo chown ${user_name}: /home/${user_name}/Desktop/opening_deeplabcut.odt"
+fi
+
+if [ ! -L "${user_name}/Desktop/mater-terminal.desktop" ]; then 
+    "ln -s /home/ubuntu/Desktop/mate-terminal.desktop /home/${user_name}/Desktop/."
+    "sudo chown ${user_name}: /home/${user_name}/Desktop/mate-terminal.desktop"
+fi
+
+
+# Copy shared Python environment to user's local file
+# ASK: IS THIS ONLY FOR TESTING PURPOSED THAT WE HARDCODED LMCN644 INTO THIS? 
+# ASK: We should probably replace lmcn644 with ubuntu because we installed the python env for him?
+sudo cp /home/lmcn644/.bashrc /home/${user_name}/Desktop/.bashrc
+sudo cp /home/lmcn644/.bash_profile /home/${user_name}/Desktop/.bash_profile
